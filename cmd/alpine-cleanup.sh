278b1440a38dd6e327e96f43b22c975427db84f9
sebthom <sebthom@users.noreply.github.com>
2025-05-15 23:26:40 +0200

    docs: update copyright notice

--- cmd/alpine-cleanup.sh
+++ cmd/alpine-cleanup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# Copyright 2021-2022 by Vegard IT GmbH, Germany, https://vegardit.com
+# SPDX-FileCopyrightText: © Vegard IT GmbH (https://vegardit.com)
+# SPDX-FileContributor: Sebastian Thomschke
 # SPDX-License-Identifier: Apache-2.0
-#
-# Author: Sebastian Thomschke, Vegard IT GmbH
-#
+# SPDX-ArtifactOfProjectHomePage: https://github.com/vegardit/docker-shared
+
 set -e
 
 echo "#################################################"
--- lib/build-image-init.sh
+++ lib/build-image-init.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 #
-# Copyright 2021-2022 by Vegard IT GmbH, Germany, https://vegardit.com
+# SPDX-FileCopyrightText: © Vegard IT GmbH (https://vegardit.com)
+# SPDX-FileContributor: Sebastian Thomschke
 # SPDX-License-Identifier: Apache-2.0
-#
-# Author: Sebastian Thomschke, Vegard IT GmbH
-#
+# SPDX-ArtifactOfProjectHomePage: https://github.com/vegardit/docker-shared
 
 source $(dirname ${BASH_SOURCE[0]})/bash-init.sh
 
